e5a7bb998029c17a724709990c15930ea9c430c3
Rahul Boda <rahul.fall15@gmail.com>
2018-03-26 22:30:15 -0500

    refactor for keystone package structure

--- Master/module1/src/main/java/com/hcsc/claim/module1/repository/ClaimRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hcsc.claim.module1.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hcsc.claim.module1.model.Resource;
-
-
-public interface ClaimRepository extends CrudRepository<Resource, Long>{}
